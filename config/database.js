e5f32b39897ba85378d598048c5aaad5f3add191
Dhruvi <dhruvipatoliya@example.com>
2023-07-13 17:47:49 +0530

    database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const db = mongoose.connection
-mongoose.connect('mongodb://127.0.0.1/admin')
+mongoose.connect('mongodb+srv://dhruvipatoliya:Dhruvi612004@cluster0.fmm0ehu.mongodb.net/Project')
 db.once('open',(err)=>{
     if(err){
         console.log(err,'database is not connected');
